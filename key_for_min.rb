72339f960293789206c363c07185bd5635156b67
Otabek Kamalov <otakam86@gmail.com>
2018-07-23 13:55:07 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,34 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+ if name_hash == {}
+    nil 
+ elsif
+    #creating array of values 
+    arr_values = name_hash.collect do |key,value|
+                 value
+                 end
+    #initializing the smallest key 
+    s_key = nil 
+    name_hash.each do |key, value|
+            if arr_values[0] == value 
+             s_key =  key 
+            end
+          end
+    
+    #comparing and assigning to key of the smallest hash value 
+    arr_values.each do |element|
+  
+      name_hash.each do |key,value|
+         if element > value
+         s_key = key 
+         end
+      end
+    end
+ end
+s_key
 end
+
+
+
+#puts key_for_min_value({:blake => 10, :ashley => 50, :adam => 17})
