641f5738e715cb622f2aa9e3b7d70994f11aee02
Linh Huynh <35814458+linh4@users.noreply.github.com>
2018-07-31 01:05:15 -0400

    Update key_for_min.rb

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
   return nil if name_hash.size == 0
   min_key, min_value = name_hash.first
-  name_hash.each { |key, value|
+  name_hash.each do |key, value|
     if value < min_value
       min_key = key
     end
-  }
+  end
   min_key
 end
